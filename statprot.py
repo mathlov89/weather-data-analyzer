c3e9ae1e9bfe0bb38be40ccaa3e557ed7076b179
Attila Lovas <attila.lovas@gmail.com>
2018-06-15 23:22:14 +0200

    1

---
+++ statprot.py
@@ -0,0 +1,45 @@
+"""
+This python class implements two different statistical analysis protocol for data of biometeorological origin.
+"""
+
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2_contingency as chi2
+
+class StatProt:
+    """
+    Statistical analysis protocols
+
+    """
+
+    def __init__(self):
+        pass
+
+    def prot1(self):
+        """
+        :return: analysis report
+        Protocol 1:
+        1. Pearson Chi squared test
+        2. Conditional probability calculation
+        """
+
+        def chi2test():
+            pass
+
+        def prob():
+            pass
+
+
+
+    def prot2(self):
+        """
+        :return: analysis report
+        1. Intensity estimation for sparse events
+        2. Causal analysis using CCM algorithm
+        3. Cox process regression to quantify the causal relation
+        """
+
+        pass
+
+
+
